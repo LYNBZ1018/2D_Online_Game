d93093281d3f061c4456142ba508c3ae1400d062
LYNBZ1010 <liuyunuo1018@163.com>
2022-03-15 15:23:06 +0800

    add settings css

--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -56,7 +56,7 @@ class Settings {
             </div>
         </div>
         <div class="ac-game-settings-password ac-game-settings-password-second">
-            <div class="ac-game-settings-iten">
+            <div class="ac-game-settings-item">
                 <input type="password" placeholder="确认密码">
             </div>
         </div>
@@ -70,7 +70,7 @@ class Settings {
         </div>
         <br>
         <div class="ac-game-settings-acwing">
-            <img width="30" src="">
+            <img width="30" src="https://app1832.acapp.acwing.com.cn/static/image/settings/acwing_logo.jpg">
             <br>
             <div>
                 AcWing一键登录
@@ -79,13 +79,78 @@ class Settings {
     </div>
 </div>
 `);
-        this.root.$ac_game
+        this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login_username = this.$login.find(".ac-game-settings-username input");
+        this.$login_password = this.$login.find(".ac-game-settings-password input");
+        this.$login_submit = this.$login.find(".ac-game-settings-submit-button");
+        this.$login_error_message = this.$login.find(".ac-game-settings-error-message");
+        this.$login_register = this.$login.find(".ac-game-settings-option");
+
+        this.$login.hide();
+
+        this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register_username = this.$register.find(".ac-game-settings-username input");
+        this.$register_password = this.$register.find(".ac-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".ac-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".ac-game-settings-submit button");
+        this.$register_error_message = this.$register.find(".ac-game-settings-error-message");
+        this.$register_login = this.$register.find(".ac-game-settings-option");
+
+        this.$register.hide();
+
+        this.root.$ac_game.append(this.$settings);
 
         this.start();
     }
 
     start() {
         this.getinfor();
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+    }
+
+    add_listening_events_login() {
+        let outer = this;
+        this.$login_register.click(function() {
+            outer.register();  // 跳转到注册界面
+        });
+        this.$login_submit.click(function() {
+            outer.login_on_remote();
+        });
+    }
+
+    login_on_remote() {
+        let outer = this;
+        let username = this.$login_username.val();
+        let password = this.$login_password.val();
+        this.$login_error_message.empty();
+        $ajax({
+            url: "https://app1832.acapp.acwing.com.cn/settings/login/",
+            type: "GET",
+            data: {
+                username: username,
+                password: password,
+            },
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    location.reload();
+                } else {
+                    outer.$login_error_message.html(resp.result);
+                }
+            }
+        });
+    }
+
+    add_lestening_events_register() {
+        let outer = this;
+        this.$register_login.click(function() {
+            outer.login();  // 跳转到登陆界面
+        });
     }
 
     register() {
